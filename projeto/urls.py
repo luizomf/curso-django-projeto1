0ee38413bddf0b3d42dde24dbd6865493be74c18
Luiz Otávio Miranda <otaviomirandabr19@gmail.com>
2021-11-11 10:07:13 -0300

    Entendendo urls.py e uma função de view

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def my_view(request):
+    return HttpResponse('UM LINDA STRING')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view),
 ]
