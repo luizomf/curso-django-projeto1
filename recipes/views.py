6ba2867e7a51f33f6df4e381ffb71674c2991ae9
Luiz Otávio Miranda <otaviomirandabr19@gmail.com>
2021-11-11 17:54:24 -0300

    Exemplo de template temp

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Luiz Otávio',
+    })
 
 
 def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'me-apague/temp.html')
 
 
 def sobre(request):
