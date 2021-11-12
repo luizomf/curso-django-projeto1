2850074e3ac5cd338b4d81a417cac079f20826cd
Luiz Otávio Miranda <otaviomirandabr19@gmail.com>
2021-11-12 07:43:46 -0300

    Removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
-    path('', home),  # Home
-    path('sobre/', sobre),  # /sobre/
-    path('contato/', contato),  # /contato/
+    path('', home),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,11 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Luiz Otávio',
     })
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
