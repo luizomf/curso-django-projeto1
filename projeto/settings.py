8ce654ed4cd64f2149326992cef090df345b2932
Luiz Otávio Miranda <otaviomirandabr19@gmail.com>
2021-11-13 22:06:52 -0300

    STATIC_URL e STATICFILES_DIRS no Django

--- projeto/settings.py
+++ projeto/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
