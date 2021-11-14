3ca96deffb6242c8a7e74bebee6a40b5637af52b
Luiz Otávio Miranda <otaviomirandabr19@gmail.com>
2021-11-14 07:09:43 -0300

    Trabalhando nas diferenças entre detail page e list page

--- recipes/views.py
+++ recipes/views.py
@@ -11,4 +11,5 @@ def home(request):
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
+        'is_detail_page': True,
     })
