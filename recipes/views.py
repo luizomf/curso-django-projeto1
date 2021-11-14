288bafc899a1ce9dcecf66a605be6e1b537b250f
Luiz Otávio Miranda <otaviomirandabr19@gmail.com>
2021-11-14 06:23:12 -0300

    Injetando dados na lista da view Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Luiz Otávio',
+        'recipe': make_recipe(),
     })
---
+++ utils/recipes/factory.py
@@ -0,0 +1,40 @@
+# from inspect import signature
+from random import randint
+
+from faker import Faker
+
+
+def rand_ratio():
+    return randint(840, 900), randint(473, 573)
+
+
+fake = Faker('pt_BR')
+# print(signature(fake.random_number))
+
+
+def make_recipe():
+    return {
+        'title': fake.sentence(nb_words=6),
+        'description': fake.sentence(nb_words=12),
+        'preparation_time': fake.random_number(digits=2, fix_len=True),
+        'preparation_time_unit': 'Minutos',
+        'servings': fake.random_number(digits=2, fix_len=True),
+        'servings_unit': 'Porção',
+        'preparation_steps': fake.text(3000),
+        'created_at': fake.date_time(),
+        'author': {
+            'first_name': fake.first_name(),
+            'last_name': fake.last_name(),
+        },
+        'category': {
+            'name': fake.word()
+        },
+        'cover': {
+            'url': 'https://loremflickr.com/%s/%s/food,cook' % rand_ratio(),
+        }
+    }
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(make_recipe())
