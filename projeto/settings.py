7668ac61742bb985d9c99afc30cb9a8a78f9deae
Luiz Otávio Miranda <otaviomirandabr19@gmail.com>
2021-11-13 23:30:53 -0300

    STATIC_ROOT

--- projeto/settings.py
+++ projeto/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
