e31be4435bb67bc7a70b4c31dd1b024b707df22a
Luiz Otávio Miranda <otaviomirandabr19@gmail.com>
2021-11-14 22:40:47 -0300

    Iniciando a criação do primeiro model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+
+# EDITED
+# title description slug
+# preparation_time preparation_time_unit
+# servings servings_unit
+# preparation_step
+# preparation_step_is_html
+# created_at updated_at
+# is_published
+# cover
+# category (Relação)
+# Author (Relação)
