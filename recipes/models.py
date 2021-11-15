0695666e84f6c2d9c8930bb31583bf5e50e0a4f2
Luiz Otávio Miranda <otaviomirandabr19@gmail.com>
2021-11-14 23:41:31 -0300

    makemigrations e migrate - Aplicando as migrações

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 
 class Recipe(models.Model):
@@ -15,15 +20,9 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-
-# EDITED
-# title description slug
-# preparation_time preparation_time_unit
-# servings servings_unit
-# preparation_step
-# preparation_step_is_html
-# created_at updated_at
-# is_published
-# cover
-# category (Relação)
-# Author (Relação)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
