f5e1879c85eac16c995aa0d5135528c3b1b8dc36
Luiz Otávio <luizomf@gmail.com>
2021-11-18 13:59:34 -0300

    Filtrando is_published

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,9 @@ from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
@@ -13,9 +15,10 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True,
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
