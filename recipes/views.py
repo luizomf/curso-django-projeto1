fd0d8d24099b36bd991d176370b52ad569650a0e
Luiz Otávio <luizomf@gmail.com>
2021-11-22 15:54:35 -0300

    Mostrando a página de detalhes da receita

--- recipes/views.py
+++ recipes/views.py
@@ -28,7 +28,12 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
