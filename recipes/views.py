285713c5f4c0b93961d09aa705e3a2fd3d4b6361
Luiz Otávio <luizomf@gmail.com>
2021-11-23 10:45:06 -0300

    Usar get_list_or_404 na home ou não?

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
-from utils.recipes.factory import make_recipe
 
 from recipes.models import Recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(
-        is_published=True
+        is_published=True,
     ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
