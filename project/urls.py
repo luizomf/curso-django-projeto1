b126d32201ca305edaac5c0b9154bf766cc9299a
Luiz Otávio <luizomf@gmail.com>
2021-11-24 08:10:34 -0300

    Renomeando um projeto Django inteiro

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""projeto URL Configuration
+"""project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
