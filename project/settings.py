b8ca687466df1c079e0b5a3e5c30bcf39e792733
Luiz Otávio <luizomf@gmail.com>
2021-11-24 08:51:36 -0300

    Internacionalização, Timezones e Tradução do Django para português - PT-BR

--- project/settings.py
+++ project/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
