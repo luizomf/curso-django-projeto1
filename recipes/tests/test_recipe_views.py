d3d82661d1fdd5cac4442195e8f3987da2492c46
Luiz Otávio <luizomf@gmail.com>
2021-11-26 09:37:22 -0300

    Testando o context e content do template

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -47,7 +47,15 @@ class RecipeViewsTest(TestCase):
             preparation_steps_is_html=False,
             is_published=True,
         )
-        assert 1 == 1
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
