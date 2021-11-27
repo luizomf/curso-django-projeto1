8c7f6099021f98948739a6ceb0eaa9290156eecf
Luiz Otávio <luizomf@gmail.com>
2021-11-27 11:40:46 -0300

    Preparando para testar os models

---
+++ recipes/tests/test_recipe_model_recipe.py
@@ -0,0 +1,11 @@
+from .test_recipe_base import RecipeTestBase
+
+
+class RecipeModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    def test_the_test(self):
+        recipe = self.recipe
+        ...
