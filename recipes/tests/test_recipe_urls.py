e1c5704bfd88c206b69d64ef5c8bf2f52e742a54
Luiz Otávio <luizomf@gmail.com>
2021-12-01 08:44:38 -0300

    Criando uma nova URL para search com TDD

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -14,3 +14,9 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
+
+    def test_recipe_search_url_is_correct(self):
+        url = reverse('recipes:search')
+        self.assertEqual(url, '/recipes/search/')
+
+# RED - GREEN - REFACTOR
--- recipes/urls.py
+++ recipes/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('recipes/category/<int:category_id>/',
          views.category, name="category"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path('recipes/search/', lambda request: ..., name="search"),
 ]
