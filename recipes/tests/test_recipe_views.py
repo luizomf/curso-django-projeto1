cb56325d5604a860dfa8b20fd1bb7e196c18f2d5
Luiz Otávio <luizomf@gmail.com>
2021-12-01 10:14:45 -0300

    Levantando erro 404 se o usuário não enviar o termo de busca

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -137,3 +137,8 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_search_loads_correct_template(self):
         response = self.client.get(reverse('recipes:search'))
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        url = reverse('recipes:search')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
@@ -37,4 +38,9 @@ def recipe(request, id):
 
 
 def search(request):
+    search_term = request.GET.get('q')
+
+    if not search_term:
+        raise Http404()
+
     return render(request, 'recipes/pages/search.html')
