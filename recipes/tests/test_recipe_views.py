1d733071261c9094897aa28c2abaf7bd87dcc66c
Luiz Otávio <luizomf@gmail.com>
2021-12-02 06:40:20 -0300

    Testando se o search_term está escapado

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -142,3 +142,11 @@ class RecipeViewsTest(RecipeTestBase):
         url = reverse('recipes:search')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_search_term_is_on_page_title_and_escaped(self):
+        url = reverse('recipes:search') + '?q=<Teste>'
+        response = self.client.get(url)
+        self.assertIn(
+            'Search for &quot;&lt;Teste&gt;&quot;',
+            response.content.decode('utf-8')
+        )
