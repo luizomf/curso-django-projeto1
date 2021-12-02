4ed12b5bddef1ed22b89125df0c07870198b33e7
Luiz Otávio <luizomf@gmail.com>
2021-12-02 09:50:51 -0300

    Add is_published

--- recipes/views.py
+++ recipes/views.py
@@ -45,8 +45,11 @@ def search(request):
         raise Http404()
 
     recipes = Recipe.objects.filter(
-        Q(title__icontains=search_term) |
-        Q(description__icontains=search_term),
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
     ).order_by('-id')
 
     return render(request, 'recipes/pages/search.html', {
