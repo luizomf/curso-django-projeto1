35e6196d06df3dc66614bb427918a80a14ad13fe
Luiz Otávio <luizomf@gmail.com>
2021-12-06 11:31:14 -0300

    Enviando post para outra URL

--- authors/urls.py
+++ authors/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'authors'
+
 urlpatterns = [
     path('register/', views.register_view, name='register'),
+    path('register/create/', views.register_create, name='create'),
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
 
 from .forms import RegisterForm
 
 
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
