258e6964efea60485ef7a39cfe57b38336e76e9d
Luiz Otávio <luizomf@gmail.com>
2021-12-06 10:36:47 -0300

    Campos manualmente

---
+++ authors/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+        ]
--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .forms import RegisterForm
+
 
 def register_view(request):
-    return render(request, 'authors/pages/register_view.html')
+    if request.POST:
+        form = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+    })
