303ff7ce0ad476ab3a873e65ad4763486d25ac9e
Luiz Otávio <luizomf@gmail.com>
2021-12-08 07:23:46 -0300

    is_valid and save

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
 
@@ -19,5 +20,11 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del(request.session['register_form_data'])
 
     return redirect('authors:register')
