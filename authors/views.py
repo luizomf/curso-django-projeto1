03050a00ed73a9c9898334c8baa0a248fdc2e1be
Luiz Otávio <luizomf@gmail.com>
2021-12-10 08:38:29 -0300

    Corrigindo criptografia de senha

--- authors/views.py
+++ authors/views.py
@@ -22,7 +22,9 @@ def register_create(request):
     form = RegisterForm(POST)
 
     if form.is_valid():
-        form.save()
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
         messages.success(request, 'Your user is created, please log in.')
 
         del(request.session['register_form_data'])
