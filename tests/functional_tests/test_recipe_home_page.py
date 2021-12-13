3808c866654058c6124d5e9a43cc723c7581b2ba
Luiz Otávio <luizomf@gmail.com>
2021-12-13 15:32:11 -0300

    Criando a classe base

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -5,14 +5,21 @@ from selenium.webdriver.common.by import By
 from utils.browser import make_chrome_browser
 
 
-class RecipeHomePageFunctionalTest(StaticLiveServerTestCase):
+class RecipeBaseFunctionalTest(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
     def sleep(self, seconds=5):
         time.sleep(seconds)
 
-    def test_the_test(self):
-        browser = make_chrome_browser()
-        browser.get(self.live_server_url)
-        self.sleep(6)
-        body = browser.find_element(By.TAG_NAME, 'body')
+
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    def test_recipe_home_page_without_recipes_not_found_message(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here 🥲', body.text)
-        browser.quit()
--- utils/browser.py
+++ utils/browser.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from time import sleep
 
@@ -15,6 +16,9 @@ def make_chrome_browser(*options):
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
+
+    if os.environ.get('SELENIUM_HEADLESS') == '1':
+        chrome_options.add_argument('--headless')
 
     chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
     browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
