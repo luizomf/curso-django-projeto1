d3ecc1e39a6cbf1801cb2db25348022e2729e6c7
Luiz Otávio <luizomf@gmail.com>
2021-12-13 15:54:35 -0300

    Pytest markers

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,8 +1,10 @@
+import pytest
 from selenium.webdriver.common.by import By
 
 from .base import RecipeBaseFunctionalTest
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
