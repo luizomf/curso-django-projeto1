0bc55adba83a452819b3d4fd701311fa6b2db5eb
Luiz Otávio <luizomf@gmail.com>
2021-12-13 10:48:51 -0300

    Selenium browser factory (chrome)

---
+++ utils/browser.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+ROOT_PATH = Path(__file__).parent.parent
+CHROMEDRIVER_NAME = 'chromedriver'
+CHROMEDRIVER_PATH = ROOT_PATH / 'bin' / CHROMEDRIVER_NAME
+
+
+def make_chrome_browser(*options):
+    chrome_options = webdriver.ChromeOptions()
+
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(executable_path=CHROMEDRIVER_PATH)
+    browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
+    return browser
+
+
+if __name__ == '__main__':
+    browser = make_chrome_browser('--headless')
+    browser.get('http://www.udemy.com/')
+    sleep(5)
+    browser.quit()
