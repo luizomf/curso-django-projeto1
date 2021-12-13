2a538adf63fc5bcabe0dcb9cb3476691f8513e7b
Luiz Otávio <luizomf@gmail.com>
2021-12-13 14:56:33 -0300

    Primeiro teste com selenium

---
+++ tests/functional_tests/test_recipe_home_page.py
@@ -0,0 +1,18 @@
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
+from utils.browser import make_chrome_browser
+
+
+class RecipeHomePageFunctionalTest(StaticLiveServerTestCase):
+    def sleep(self, seconds=5):
+        time.sleep(seconds)
+
+    def test_the_test(self):
+        browser = make_chrome_browser()
+        browser.get(self.live_server_url)
+        self.sleep(6)
+        body = browser.find_element(By.TAG_NAME, 'body')
+        self.assertIn('No recipes found here 🥲', body.text)
+        browser.quit()
