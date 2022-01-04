fc2b67ef57cbdae2650d11a83a4781ace525afca
Luiz Otávio <luizomf@gmail.com>
2022-01-04 14:36:12 -0300

    Testando o logout

---
+++ authors/tests/test_author_logout.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+
+class AuthorLogoutTest(TestCase):
+    def test_user_tries_to_logout_using_get_method(self):
+        User.objects.create_user(username='my_user', password='my_pass')
+        self.client.login(username='my_user', password='my_pass')
+
+        response = self.client.get(
+            reverse('authors:logout'),
+            follow=True
+        )
+
+        self.assertIn(
+            'Invalid logout request',
+            response.content.decode('utf-8')
+        )
+
+    def test_user_tries_to_logout_another_user(self):
+        User.objects.create_user(username='my_user', password='my_pass')
+        self.client.login(username='my_user', password='my_pass')
+
+        response = self.client.post(
+            reverse('authors:logout'),
+            data={
+                'username': 'another_user'
+            },
+            follow=True
+        )
+
+        self.assertIn(
+            'Invalid logout user',
+            response.content.decode('utf-8')
+        )
+
+    def test_user_can_logout_successfully(self):
+        User.objects.create_user(username='my_user', password='my_pass')
+        self.client.login(username='my_user', password='my_pass')
+
+        response = self.client.post(
+            reverse('authors:logout'),
+            data={
+                'username': 'my_user'
+            },
+            follow=True
+        )
+
+        self.assertIn(
+            'Logged out successfully',
+            response.content.decode('utf-8')
+        )
--- authors/views.py
+++ authors/views.py
@@ -72,10 +72,13 @@ def login_create(request):
 @login_required(login_url='authors:login', redirect_field_name='next')
 def logout_view(request):
     if not request.POST:
+        messages.error(request, 'Invalid logout request')
         return redirect(reverse('authors:login'))
 
     if request.POST.get('username') != request.user.username:
+        messages.error(request, 'Invalid logout user')
         return redirect(reverse('authors:login'))
 
+    messages.success(request, 'Logged out successfully')
     logout(request)
     return redirect(reverse('authors:login'))
