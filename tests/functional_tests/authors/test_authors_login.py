487046513fa02be6879db617e70e123dcdac0dfe
Luiz Otávio <luizomf@gmail.com>
2022-01-04 09:49:24 -0300

    Testando um login inválido

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -45,3 +45,27 @@ class AuthorsLoginTest(AuthorsBaseTest):
             'Not Found',
             self.browser.find_element(By.TAG_NAME, 'body').text
         )
+
+    def test_form_login_is_invalid(self):
+        # Usuário abre a página de login
+        self.browser.get(
+            self.live_server_url + reverse('authors:login')
+        )
+
+        # Usuário vê o formulário de login
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        # E tenta enviar valores vazios
+        username = self.get_by_placeholder(form, 'Type your username')
+        password = self.get_by_placeholder(form, 'Type your password')
+        username.send_keys(' ')
+        password.send_keys(' ')
+
+        # Envia o formulário
+        form.submit()
+
+        # Vê uma mensagem de erro na tela
+        self.assertIn(
+            'Invalid username or password',
+            self.browser.find_element(By.TAG_NAME, 'body').text
+        )
