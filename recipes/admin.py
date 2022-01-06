28a21314be21dc91c1a5b8be6942f61155d57606
Luiz Otávio <luizomf@gmail.com>
2022-01-06 14:23:25 -0300

    Preenchendo o slug automaticamente com prepopulated_fields

--- recipes/admin.py
+++ recipes/admin.py
@@ -17,6 +17,9 @@ class RecipeAdmin(admin.ModelAdmin):
     list_per_page = 10
     list_editable = 'is_published',
     ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('title',)
+    }
 
 
 admin.site.register(Category, CategoryAdmin)
