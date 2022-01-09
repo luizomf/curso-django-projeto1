c3eade81f925fa80a69fc869c377691f9e9617a5
Luiz Otávio <luizomf@gmail.com>
2022-01-09 08:55:16 -0300

    Como criei uma view para permitir criação de nvoas receitas?

--- authors/urls.py
+++ authors/urls.py
@@ -12,6 +12,11 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path(
+        'dashboard/recipe/new/',
+        views.dashboard_recipe_new,
+        name='dashboard_recipe_new'
+    ),
+    path(
         'dashboard/recipe/<int:id>/edit/',
         views.dashboard_recipe_edit,
         name='dashboard_recipe_edit'
--- authors/views.py
+++ authors/views.py
@@ -138,3 +138,34 @@ def dashboard_recipe_edit(request, id):
             'form': form
         }
     )
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard_recipe_new(request):
+    form = AuthorRecipeForm(
+        data=request.POST or None,
+        files=request.FILES or None,
+    )
+
+    if form.is_valid():
+        recipe: Recipe = form.save(commit=False)
+
+        recipe.author = request.user
+        recipe.preparation_steps_is_html = False
+        recipe.is_published = False
+
+        recipe.save()
+
+        messages.success(request, 'Salvo com sucesso!')
+        return redirect(
+            reverse('authors:dashboard_recipe_edit', args=(recipe.id,))
+        )
+
+    return render(
+        request,
+        'authors/pages/dashboard_recipe.html',
+        context={
+            'form': form,
+            'form_action': reverse('authors:dashboard_recipe_new')
+        }
+    )
