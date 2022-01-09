c578deda31063e32912e04ff251343325fc11c13
Luiz Otávio <luizomf@gmail.com>
2022-01-09 08:09:47 -0300

    Corrigindo o estilo CSS do formulário e da grid

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,8 +1,15 @@
 from django import forms
 from recipes.models import Recipe
+from utils.django_forms import add_attr
 
 
 class AuthorRecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
+        add_attr(self.fields.get('cover'), 'class', 'span-2')
+
     class Meta:
         model = Recipe
         fields = 'title', 'description', 'preparation_time', \
