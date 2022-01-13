f92495e6bdf9b531fb6783d0d522a5c4de1773af
Luiz Otávio <luizomf@gmail.com>
2022-01-13 10:02:09 -0300

    Category e Search ListView

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,8 +6,13 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.RecipeListViewHome.as_view(), name="home"),
-    path('recipes/search/', views.search, name="search"),
-    path('recipes/category/<int:category_id>/',
-         views.category, name="category"),
+    path(
+        'recipes/search/',
+        views.RecipeListViewSearch.as_view(), name="search"
+    ),
+    path(
+        'recipes/category/<int:category_id>/',
+        views.RecipeListViewCategory.as_view(), name="category"
+    ),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -41,6 +41,44 @@ class RecipeListViewHome(RecipeListViewBase):
     template_name = 'recipes/pages/home.html'
 
 
+class RecipeListViewCategory(RecipeListViewBase):
+    template_name = 'recipes/pages/category.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            category__id=self.kwargs.get('category_id')
+        )
+        return qs
+
+
+class RecipeListViewSearch(RecipeListViewBase):
+    template_name = 'recipes/pages/search.html'
+
+    def get_queryset(self, *args, **kwargs):
+        search_term = self.request.GET.get('q', '')
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            Q(
+                Q(title__icontains=search_term) |
+                Q(description__icontains=search_term),
+            )
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        search_term = self.request.GET.get('q', '')
+
+        ctx.update({
+            'page_title': f'Search for "{search_term}" |',
+            'search_term': search_term,
+            'additional_url_query': f'&q={search_term}',
+        })
+
+        return ctx
+
+
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True,
