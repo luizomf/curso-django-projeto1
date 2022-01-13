e9f91152f323936dc4fab4376c05325fbbb3a471
Luiz Otávio <luizomf@gmail.com>
2022-01-13 05:05:36 -0300

    Movendo views.py para o package views

--- authors/views/all.py
+++ authors/views/all.py
@@ -1,3 +1,5 @@
+from authors.forms import LoginForm, RegisterForm
+from authors.forms.recipe_form import AuthorRecipeForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -5,10 +7,6 @@ from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from recipes.models import Recipe
-
-from authors.forms.recipe_form import AuthorRecipeForm
-
-from .forms import LoginForm, RegisterForm
 
 
 def register_view(request):
