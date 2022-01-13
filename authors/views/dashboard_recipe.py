f184b1f35ff3795c6a56e4809ec31d0d9147671e
Luiz Otávio <luizomf@gmail.com>
2022-01-13 05:51:09 -0300

    criando o método post

--- authors/views/dashboard_recipe.py
+++ authors/views/dashboard_recipe.py
@@ -8,16 +8,37 @@ from recipes.models import Recipe
 
 
 class DashboardRecipe(View):
+    def get_recipe(self, id):
+        recipe = None
+
+        if id:
+            recipe = Recipe.objects.filter(
+                is_published=False,
+                author=self.request.user,
+                pk=id,
+            ).first()
+
+            if not recipe:
+                raise Http404()
+
+        return recipe
+
+    def render_recipe(self, form):
+        return render(
+            self.request,
+            'authors/pages/dashboard_recipe.html',
+            context={
+                'form': form
+            }
+        )
+
     def get(self, request, id):
-        recipe = Recipe.objects.filter(
-            is_published=False,
-            author=request.user,
-            pk=id,
-        ).first()
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(instance=recipe)
+        return self.render_recipe(form)
 
-        if not recipe:
-            raise Http404()
-
+    def post(self, request, id):
+        recipe = self.get_recipe(id)
         form = AuthorRecipeForm(
             data=request.POST or None,
             files=request.FILES or None,
@@ -39,10 +60,4 @@ class DashboardRecipe(View):
                 reverse('authors:dashboard_recipe_edit', args=(id,))
             )
 
-        return render(
-            request,
-            'authors/pages/dashboard_recipe.html',
-            context={
-                'form': form
-            }
-        )
+        return self.render_recipe(form)
