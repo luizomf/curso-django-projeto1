e25f19e95654655fe0f7ab063275e5067aaea7c8
Luiz Otávio <luizomf@gmail.com>
2022-01-24 10:45:55 -0300

    select_related

--- recipes/views.py
+++ recipes/views.py
@@ -23,6 +23,7 @@ class RecipeListViewBase(ListView):
         qs = qs.filter(
             is_published=True,
         )
+        qs = qs.select_related('author', 'category')
         return qs
 
     def get_context_data(self, *args, **kwargs):
