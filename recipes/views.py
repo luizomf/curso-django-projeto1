9b5bb1eb3cd3c805790ef70bdbeb2427f30c9332
Luiz Otávio <luizomf@gmail.com>
2022-01-27 08:20:02 -0300

    QuerySets são preguiçosas

--- recipes/views.py
+++ recipes/views.py
@@ -14,9 +14,17 @@ PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def theory(request, *args, **kwargs):
+    recipes = Recipe.objects.all()
+    recipes = recipes.filter(title__icontains='Teste')
+
+    context = {
+        'recipes': recipes
+    }
+
     return render(
         request,
-        'recipes/pages/theory.html'
+        'recipes/pages/theory.html',
+        context=context
     )
 
 
