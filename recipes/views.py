b4854767a5b98e5cfb5abd58ea3e1f6f88397a11
Luiz Otávio <luizomf@gmail.com>
2022-01-27 10:17:25 -0300

    Agregação

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 import os
 
-from django.db.models import F, Q
+from django.db.models import Q
+from django.db.models.aggregates import Count
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.http.response import Http404
@@ -14,10 +15,12 @@ PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def theory(request, *args, **kwargs):
-    recipes = Recipe.objects.values('id', 'title')
+    recipes = Recipe.objects.values('id', 'title')[:5]
+    number_of_recipes = recipes.aggregate(number=Count('id'))
 
     context = {
-        'recipes': recipes
+        'recipes': recipes,
+        'number_of_recipes': number_of_recipes['number']
     }
 
     return render(
