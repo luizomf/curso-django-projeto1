e9751653e442eda7a5539239b28bcac3334d8374
Luiz Otávio <luizomf@gmail.com>
2022-02-02 10:00:30 -0300

    Removendo load_dotenv do settings.py em favor do pytest-dotenv

--- project/settings.py
+++ project/settings.py
@@ -14,10 +14,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-if os.environ.get('DEBUG', None) is None:
-    from dotenv import load_dotenv
-    load_dotenv()
-
 from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
