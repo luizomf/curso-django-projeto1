dd92e59ee0222297b0766ec0c764db9b83149630
Luiz Otávio <luizomf@gmail.com>
2022-02-10 07:30:13 -0300

    Adicionando a lógica de logout

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -42,3 +42,15 @@
     buttonShowMenu.addEventListener('click', showMenu);
   }
 })();
+
+(() => {
+  const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link');
+  const formLogout = document.querySelector('.form-logout');
+
+  for (const link of authorsLogoutLinks) {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      formLogout.submit();
+    });
+  }
+})();
