c63c0e01fe69697713418d77ca69a9f635f7678c
Luiz Otávio <luizomf@gmail.com>
2022-02-10 07:16:00 -0300

    Adicionando a lógica do JavaScript

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -1,4 +1,4 @@
-function my_scope() {
+(() => {
   const forms = document.querySelectorAll('.form-delete');
 
   for (const form of forms) {
@@ -12,6 +12,33 @@ function my_scope() {
       }
     });
   }
-}
+})();
 
-my_scope();
+(() => {
+  const buttonCloseMenu = document.querySelector('.button-close-menu');
+  const buttonShowMenu = document.querySelector('.button-show-menu');
+  const menuContainer = document.querySelector('.menu-container');
+
+  const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+  const menuHiddenClass = 'menu-hidden';
+
+  const closeMenu = () => {
+    buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+    menuContainer.classList.add(menuHiddenClass);
+  };
+
+  const showMenu = () => {
+    buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+    menuContainer.classList.remove(menuHiddenClass);
+  };
+
+  if (buttonCloseMenu) {
+    buttonCloseMenu.removeEventListener('click', closeMenu);
+    buttonCloseMenu.addEventListener('click', closeMenu);
+  }
+
+  if (buttonShowMenu) {
+    buttonCloseMenu.removeEventListener('click', showMenu);
+    buttonShowMenu.addEventListener('click', showMenu);
+  }
+})();
