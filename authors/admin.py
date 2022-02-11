73cdd010d75d78008f09b311fdc4c03ac13e5fb8
Luiz Otávio <luizomf@gmail.com>
2022-02-11 07:07:53 -0300

    Criando o model de profiles

--- authors/admin.py
+++ authors/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from authors.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    ...
--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default='', blank=True)
