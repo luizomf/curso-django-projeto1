a5b47adf58cd2191fd5e1ef6e4dd7b3a382da339
Luiz Otávio <luizomf@gmail.com>
2022-02-23 07:22:17 -0300

    Atualizando a cover

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,6 +1,6 @@
 import os
 
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 
 from recipes.models import Recipe
@@ -17,3 +17,12 @@ def delete_cover(instance):
 def recipe_cover_delete(sender, instance, *args, **kwargs):
     old_instance = Recipe.objects.get(pk=instance.pk)
     delete_cover(old_instance)
+
+
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.get(pk=instance.pk)
+    is_new_cover = old_instance.cover != instance.cover
+
+    if is_new_cover:
+        delete_cover(old_instance)
