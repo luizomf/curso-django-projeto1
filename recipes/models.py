5350b752edd0dbc4b108568b349dcbf82d4843c0
Luiz Otávio <luizomf@gmail.com>
2022-02-23 07:59:51 -0300

    Corrigindo a ordem do save

--- recipes/models.py
+++ recipes/models.py
@@ -89,13 +89,15 @@ class Recipe(models.Model):
             slug = f'{slugify(self.title)}'
             self.slug = slug
 
+        saved = super().save(*args, **kwargs)
+
         if self.cover:
             try:
-                self.resize_image(self.cover, 800)
+                self.resize_image(self.cover, 840)
             except FileNotFoundError:
                 ...
 
-        return super().save(*args, **kwargs)
+        return saved
 
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list)
