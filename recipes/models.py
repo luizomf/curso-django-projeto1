809b94b90e8d449d44f1a501da03b2740b953323
Luiz Otávio <luizomf@gmail.com>
2022-02-23 07:50:36 -0300

    Redimensionando imagens com o Pillow

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,7 @@
+import os
 from collections import defaultdict
 
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import F, Value
@@ -8,6 +10,7 @@ from django.forms import ValidationError
 from django.urls import reverse
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
+from PIL import Image
 from tag.models import Tag
 
 
@@ -62,10 +65,35 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse('recipes:recipe', args=(self.id,))
 
+    @staticmethod
+    def resize_image(image, new_width=800):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_height = image_pillow.size
+
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=50,
+        )
+
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 800)
+            except FileNotFoundError:
+                ...
 
         return super().save(*args, **kwargs)
 
