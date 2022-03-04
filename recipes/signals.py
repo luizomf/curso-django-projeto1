bdbb60a3f2d5759f4531a11b1c14c363c0c596ad
Luiz Otávio <luizomf@gmail.com>
2022-03-04 07:36:17 -0300

    Corrigindo signals.py de recipes

--- recipes/signals.py
+++ recipes/signals.py
@@ -15,13 +15,19 @@ def delete_cover(instance):
 
 @receiver(pre_delete, sender=Recipe)
 def recipe_cover_delete(sender, instance, *args, **kwargs):
-    old_instance = Recipe.objects.get(pk=instance.pk)
-    delete_cover(old_instance)
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+
+    if old_instance:
+        delete_cover(old_instance)
 
 
 @receiver(pre_save, sender=Recipe)
 def recipe_cover_update(sender, instance, *args, **kwargs):
-    old_instance = Recipe.objects.get(pk=instance.pk)
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+
+    if not old_instance:
+        return
+
     is_new_cover = old_instance.cover != instance.cover
 
     if is_new_cover:
