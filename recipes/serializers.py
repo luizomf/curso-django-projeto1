1cb6b1ad138dce38e276ea7a46ee945c20651f79
Luiz Otávio <luizomf@gmail.com>
2022-03-22 10:16:29 -0300

    Primeiro serializer

---
+++ recipes/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+
+class RecipeSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=65)
+    description = serializers.CharField(max_length=165)
