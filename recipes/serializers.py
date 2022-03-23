b4236204890133dabb8b480521646595cf4418f2
Luiz Otávio <luizomf@gmail.com>
2022-03-23 03:50:38 -0300

    Renomeando campos

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -5,3 +5,10 @@ class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
+    public = serializers.BooleanField(source='is_published')
+    preparation = serializers.SerializerMethodField(
+        method_name='any_method_name'
+    )
+
+    def any_method_name(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
