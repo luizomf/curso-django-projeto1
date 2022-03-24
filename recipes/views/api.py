b8ebe07fc655c4f05c7740e36b3a450fb98430a5
Luiz Otávio <luizomf@gmail.com>
2022-03-24 07:59:41 -0300

    Métodos HTTP

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from tag.models import Tag
@@ -7,15 +8,18 @@ from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
 
 
-@api_view()
+@api_view(http_method_names=['get', 'post'])
 def recipe_api_list(request):
-    recipes = Recipe.objects.get_published()[:10]
-    serializer = RecipeSerializer(
-        instance=recipes,
-        many=True,
-        context={'request': request},
-    )
-    return Response(serializer.data)
+    if request.method == 'GET':
+        recipes = Recipe.objects.get_published()[:10]
+        serializer = RecipeSerializer(
+            instance=recipes,
+            many=True,
+            context={'request': request},
+        )
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        return Response('POST', status=status.HTTP_201_CREATED)
 
 
 @api_view()
