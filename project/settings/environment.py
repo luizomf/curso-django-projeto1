66865aa20e988121b9c1f263ddef56b39c6c02e1
Luiz Otávio <luizomf@gmail.com>
2022-03-26 05:59:50 -0300

    noqa

--- project/settings/environment.py
+++ project/settings/environment.py
@@ -28,6 +28,6 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',  # noqa
     'PAGE_SIZE': 10,
 }
