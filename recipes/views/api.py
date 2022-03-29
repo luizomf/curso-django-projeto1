046be35d0fd2dcda6c394c39d8589f67339f9460
Luiz Otávio <luizomf@gmail.com>
2022-03-29 06:14:56 -0300

    Sobrescrevendo get_queryset

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,3 +1,5 @@
+from unicodedata import category
+
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
@@ -17,6 +19,27 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+
+    def get_serializer_class(self):
+        return super().get_serializer_class()
+
+    def get_serializer(self, *args, **kwargs):
+        return super().get_serializer(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["example"] = 'this is in context now'
+        return context
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        category_id = self.request.query_params.get('category_id', '')
+
+        if category_id != '' and category_id.isnumeric():
+            qs = qs.filter(category_id=category_id)
+
+        return qs
 
     def partial_update(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
