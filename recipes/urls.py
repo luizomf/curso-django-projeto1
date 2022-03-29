97cce45dc80bec19028fec84d28f3d6cf75f9a0d
Luiz Otávio <luizomf@gmail.com>
2022-03-29 05:36:50 -0300

    Routers no Django Rest Framework

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
 from recipes import views
 
 app_name = 'recipes'
+
+recipe_api_v2_router = SimpleRouter()
+recipe_api_v2_router.register(
+    'recipes/api/v2',
+    views.RecipeAPIv2ViewSet,
+    basename='recipes-api',
+)
 
 urlpatterns = [
     path(
@@ -46,25 +54,9 @@ urlpatterns = [
         name='theory',
     ),
     path(
-        'recipes/api/v2/',
-        views.RecipeAPIv2ViewSet.as_view({
-            'get': 'list',
-            'post': 'create',
-        }),
-        name='recipes_api_v2',
-    ),
-    path(
-        'recipes/api/v2/<int:pk>/',
-        views.RecipeAPIv2ViewSet.as_view({
-            'get': 'retrieve',
-            'patch': 'partial_update',
-            'delete': 'destroy',
-        }),
-        name='recipes_api_v2_detail',
-    ),
-    path(
         'recipes/api/v2/tag/<int:pk>/',
         views.tag_api_detail,
         name='recipes_api_v2_tag',
     ),
+    path('', include(recipe_api_v2_router.urls)),
 ]
