51f52e124cafb71f166a6e0b2655972a7a91dbc5
Luiz Otávio <luizomf@gmail.com>
2022-03-30 16:47:20 -0300

    Sobrescrevendo create

--- project/settings/rest_framework.py
+++ project/settings/rest_framework.py
@@ -7,6 +7,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 
--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -63,6 +64,17 @@ class RecipeAPIv2ViewSet(ModelViewSet):
         print('REQUEST', request.user)
         print(request.user.is_authenticated)
         return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
 
     def partial_update(self, request, *args, **kwargs):
         recipe = self.get_object()
