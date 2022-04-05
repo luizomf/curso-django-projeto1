b3da0c85d1a6203f002dbccb1462879a4efb4521
Luiz Otávio <luizomf@gmail.com>
2022-04-05 09:28:37 -0300

    Criando um teste logado com JWT no Django Rest Framework

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -29,6 +29,17 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         )
         return response.data.get('access')
 
+    def get_recipe_raw_data(self):
+        return {
+            'title': 'This is the title',
+            'description': 'This is the description',
+            'preparation_time': 1,
+            'preparation_time_unit': 'Minutes',
+            'servings': '1',
+            'servings_unit': 'Person',
+            'preparation_steps': 'This is the preparation steps.'
+        }
+
     def test_recipe_api_list_returns_status_code_200(self):
         response = self.get_recipe_api_list()
         self.assertEqual(
@@ -100,5 +111,15 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
             401
         )
 
-    def test_jwt_login(self):
-        print(self.get_jwt_access_token())
+    def test_recipe_api_list_logged_user_can_create_a_recipe(self):
+        data = self.get_recipe_raw_data()
+        response = self.client.post(
+            self.get_recipe_reverse_url(),
+            data=data,
+            HTTP_AUTHORIZATION=f'Bearer {self.get_jwt_access_token()}'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            201
+        )
