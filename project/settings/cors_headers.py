561dfebffad5bb7df6700a1659b97ec27d4efa14
Luiz Otávio <luizomf@gmail.com>
2022-04-16 09:20:22 -0300

    Configurando o dotenv

--- project/settings/cors_headers.py
+++ project/settings/cors_headers.py
@@ -1,3 +1,5 @@
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:5500',
-]
+from utils.environment import get_env_variable, parse_comma_sep_str_to_list
+
+CORS_ALLOWED_ORIGINS = parse_comma_sep_str_to_list(
+    get_env_variable('CORS_ALLOWED_ORIGINS')
+)
